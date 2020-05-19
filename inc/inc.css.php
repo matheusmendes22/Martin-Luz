fbe1cc042f6a7aeaeebddd5f00f7c22aae0b6f66
Matheus Mendes <matheus.mendes2298@gmail.com>
2020-05-18 22:35:27 -0300

    Primeira seção Desktop criada e finalizada

---
+++ inc/inc.css.php
@@ -0,0 +1,2 @@
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+<link rel="stylesheet" href="assets/css/style.css">
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'inc/inc.bootstrap.php'; ?>
+<?php include 'inc/inc.css.php'; ?>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 header__text-block order-lg-1 order-2">
+                <h2>Reembolsos corporativos de forma simples e rápida. <strong>Como deve ser.</strong></h2>
+                <p>Acabe com a dor de cabeça da gestão de despesas do dia a dia da sua empresa: pagamentos, reembolsos, compras on-line e gestão do fundo fixo. Controle seus gastos escaneando notas fiscais, acompanhando aprovações, visualizando relatórios e muito mais!</p>
+                <a href="#">PEÇA JÁ SUA PROPOSTA</a>
+                <p class="header__text-info">Responderemos <strong>em até 24h.</strong></p>
+            </div>
+            <div class="col-lg-6 order-lg-2 order-1">
+                <img class="img-fluid" src="assets/img/grafico-1.png" alt="">
+            </div>
+        </div>
+    </div>
+</body>
