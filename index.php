6aa29eac6e3a02c8ec2bbc4c391dd87ed23a45e8
Matheus Mendes <matheus.mendes2298@gmail.com>
2020-05-22 10:14:38 -0300

    Desktop e Mobile finalizados, restando somente posts do wp

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 header__text-block order-lg-1 order-2">
-                <h2>Reembolsos corporativos de forma simples e rápida. <strong>Como deve ser.</strong></h2>
+                <h2>Reembolsos corporativos<br> de forma simples e rápida. <strong>Como deve ser.</strong></h2>
                 <p>Acabe com a dor de cabeça da gestão de despesas do dia a dia da sua empresa: pagamentos, reembolsos,
                     compras on-line e gestão do fundo fixo. Controle seus gastos escaneando notas fiscais, acompanhando
                     aprovações, visualizando relatórios e muito mais!</p>
@@ -37,6 +37,7 @@
                 <p>O BPP Corp é uma solução única para a administração de gastos corporativos que conta com uma conta
                     digital empresarial grátis, cartões VISA pré-pagos para controle total das despesas e um aplicativo
                     sem custos adicionais para tornar seu controle financeiro mais simples e prático!</p>
+                <a href="#">PEÇA JÁ SUA PROPOSTA</a>
             </div>
         </div>
         <div class="row cta__container">
@@ -62,40 +63,229 @@
                     reduzindo custos e aumentando os resultados da sua empresa.</p>
             </div>
             <div class="row tips-icons__container">
-            <div class="col-lg-4 tips__block">
-                <img src="assets/img/icons/laptop-rocket.png" alt="">
-                <h2>Mais rapidez na gestão das suas despesas</h2>
-                <p>Para que seu negócio cresça de maneira inteligente e rápida, substitua os processos demorados de
-                    reembolsos, aprovações, criação de relatórios e etc. por uma plataforma que te oferece tudo isso de
-                    forma rápida e digital.</p>
+                <div class="tips__block">
+                    <img class="img-fluid" src="assets/img/icons/laptop-rocket.png" alt="">
+                    <div class="tips__block-text">
+                        <h2>Mais rapidez na gestão<br> das suas despesas</h2>
+                        <p>Para que seu negócio cresça de maneira inteligente e rápida, substitua os processos demorados
+                            de
+                            reembolsos, aprovações, criação de relatórios e etc. por uma plataforma que te oferece tudo
+                            isso de
+                            forma rápida e digital.</p>
+                    </div>
+                </div>
+                <div class="tips__block">
+                    <img class="img-fluid" src="assets/img/icons/graphic.png" alt="">
+                    <div class="tips__block-text">
+                        <h2>Os relatórios que você precisa em um único lugar</h2>
+                        <p>Encontre relatórios detalhados sobre as utilizações de cada cartão corporativo cadastrado em
+                            sua
+                            equipe. Confira o saldo de cada cartão, redistribua valores, faça transferências
+                            instantâneas e não
+                            perca tempo com formulários e planilhas.</p>
+                    </div>
+                </div>
+                <div class="tips__block">
+                    <img class="img-fluid" src="assets/img/icons/wallet.png" alt="">
+                    <div class="tips__block-text">
+                        <h2>Organize todos os seus comprovantes</h2>
+                        <p>Recebendo os comprovantes de gastos de seus colaboradores através da plataforma, você não
+                            precisa mais guardar comprovantes e notas fiscais em lugar nenhum. Os gastos podem ser
+                            classificados por tipo ou por projeto, da forma que você precisar.</p>
+                    </div>
+                </div>
+                <div class="tips__block">
+                    <img class="img-fluid" src="assets/img/icons/phone.png" alt="">
+                    <div class="tips__block-text">
+                        <h2>Apresente suas despesas com um só clique</h2>
+                        <p>Enviar seus comprovantes é mais simples do que você imagina. Basta que o colaborador tire uma
+                            foto do cupom fiscal com seu smartphone através do aplicativo BPP Card. O gestor das
+                            despesas pode aprovar ou reprovar aquele gasto através do App.</p>
+                    </div>
+                </div>
+                <div class="tips__block">
+                    <img class="img-fluid" src="assets/img/icons/card.png" alt="">
+                    <div class="tips__block-text">
+                        <h2>Controle seus gastos com cartões pré-pagos VISA</h2>
+                        <p>Os cartões pré-pago BPP Corp são fáceis de controlar: o colaborador só gasta o que a empresa
+                            disponibiliza para compras online ou em lojas físicas. Conte com cartões VISA aceitos em
+                            mais de 30 milhões de estabelecimentos no mundo todo, incluindo sites e aplicativos!</p>
+                    </div>
+                </div>
+                <div class="tips__block">
+                    <img class="img-fluid" src="assets/img/icons/id.png" alt="">
+                    <div class="tips__block-text">
+                        <h2>Conta digital gratuita e exclusiva para sua empresa</h2>
+                        <p>Oferecemos uma conta digital gratuita especialmente para gerenciar despesas. Transfira o
+                            saldo de sua conta para os cartões de maneira instantânea, sempre livre de taxas de
+                            manutenção ou de juros.</p>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4 tips__block">
-                <img src="assets/img/icons/graphic.png" alt="">
-                <h2>Os relatórios que você precisa em um único lugar</h2>
-                <p>Encontre relatórios detalhados sobre as utilizações de cada cartão corporativo cadastrado em sua
-                    equipe. Confira o saldo de cada cartão, redistribua valores, faça transferências instantâneas e não
-                    perca tempo com formulários e planilhas.</p>
+            <div class="row tips__carousel">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="tips__block carousel-item active">
+                            <img class="img-fluid" src="assets/img/icons/laptop-rocket.png" alt="">
+                            <div class="tips__block-text">
+                                <h2>Mais rapidez na gestão<br> das suas despesas</h2>
+                                <p>Para que seu negócio cresça de maneira inteligente e rápida, substitua os processos
+                                    demorados
+                                    de
+                                    reembolsos, aprovações, criação de relatórios e etc. por uma plataforma que te
+                                    oferece tudo
+                                    isso de
+                                    forma rápida e digital.</p>
+                            </div>
+                        </div>
+                        <div class="tips__block carousel-item">
+                            <img class="img-fluid" src="assets/img/icons/graphic.png" alt="">
+                            <div class="tips__block-text">
+                                <h2>Os relatórios que você precisa em um único lugar</h2>
+                                <p>Encontre relatórios detalhados sobre as utilizações de cada cartão corporativo
+                                    cadastrado em
+                                    sua
+                                    equipe. Confira o saldo de cada cartão, redistribua valores, faça transferências
+                                    instantâneas e não
+                                    perca tempo com formulários e planilhas.</p>
+                            </div>
+                        </div>
+                        <div class="tips__block carousel-item">
+                            <img class="img-fluid" src="assets/img/icons/wallet.png" alt="">
+                            <div class="tips__block-text">
+                                <h2>Organize todos os seus comprovantes</h2>
+                                <p>Recebendo os comprovantes de gastos de seus colaboradores através da plataforma, você
+                                    não
+                                    precisa mais guardar comprovantes e notas fiscais em lugar nenhum. Os gastos podem
+                                    ser
+                                    classificados por tipo ou por projeto, da forma que você precisar.</p>
+                            </div>
+                        </div>
+                        <div class="tips__block carousel-item">
+                            <img class="img-fluid" src="assets/img/icons/phone.png" alt="">
+                            <div class="tips__block-text">
+                                <h2>Apresente suas despesas com um só clique</h2>
+                                <p>Enviar seus comprovantes é mais simples do que você imagina. Basta que o colaborador
+                                    tire uma
+                                    foto do cupom fiscal com seu smartphone através do aplicativo BPP Card. O gestor das
+                                    despesas pode aprovar ou reprovar aquele gasto através do App.</p>
+                            </div>
+                        </div>
+                        <div class="tips__block carousel-item">
+                            <img class="img-fluid" src="assets/img/icons/card.png" alt="">
+                            <div class="tips__block-text">
+                                <h2>Controle seus gastos com cartões pré-pagos VISA</h2>
+                                <p>Os cartões pré-pago BPP Corp são fáceis de controlar: o colaborador só gasta o que a
+                                    empresa
+                                    disponibiliza para compras online ou em lojas físicas. Conte com cartões VISA
+                                    aceitos em
+                                    mais de 30 milhões de estabelecimentos no mundo todo, incluindo sites e aplicativos!
+                                </p>
+                            </div>
+                        </div>
+                        <div class="tips__block carousel-item">
+                            <img class="img-fluid" src="assets/img/icons/id.png" alt="">
+                            <div class="tips__block-text">
+                                <h2>Conta digital gratuita e exclusiva para sua empresa</h2>
+                                <p>Oferecemos uma conta digital gratuita especialmente para gerenciar despesas.
+                                    Transfira o
+                                    saldo de sua conta para os cartões de maneira instantânea, sempre livre de taxas de
+                                    manutenção ou de juros.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-4 tips__block">
-                <img src="assets/img/icons/wallet.png" alt="">
-                <h2>Organize todos os seus comprovantes</h2>
-                <p>Recebendo os comprovantes de gastos de seus colaboradores através da plataforma, você não precisa mais guardar comprovantes e notas fiscais em lugar nenhum. Os gastos podem ser classificados por tipo ou por projeto, da forma que você precisar.</p>
+        </div>
+        <div class="row brands__container">
+            <div class="col-lg-6 order-lg-1 order-2">
+                <div class="row brands__block">
+                    <a target="_blank" href="https://burguerking.com.br">
+                        <img class="img-fluid" src="assets/img/bk.png" alt="">
+                    </a>
+                    <a target="_blank" href="https://www.boticario.com.br/">
+                        <img class="img-fluid" src="assets/img/boticario.png" alt="">
+                    </a>
+                    <a target="_blank" href="https://lojavirtual.cacaushow.com.br/">
+                        <img class="img-fluid" src="assets/img/cacau-show.png" alt="">
+                    </a>
+                    <a target="_blank" href="https://www.paguemenos.com.br">
+                        <img class="img-fluid" src="assets/img/pague-menos.png" alt="">
+                    </a>
+                </div>
             </div>
-            <div class="col-lg-4 tips__block">
-                <img src="assets/img/icons/phone.png" alt="">
-                <h2>Apresente suas despesas com um só clique</h2>
-                <p>Enviar seus comprovantes é mais simples do que você imagina. Basta que o colaborador tire uma foto do cupom fiscal com seu smartphone através do aplicativo BPP Card. O gestor das despesas pode aprovar ou reprovar aquele gasto através do App.</p>
+            <div class="col-lg-6 brands__text  order-lg-2 order-1">
+                <h2>Conheça quem já conta<br> com as <strong>vantagens<br>excluisivas</strong> do BPP Corp</h2>
+                <p>A empresas mais bem-sucedidas do<br> mundo, grandes e pequenas, confiam no<br> BPP Corp para reduzir
+                    custos<br> com organização dos gastos da suas empresas.</p>
             </div>
-            <div class="col-lg-4 tips__block">
-                <img src="assets/img/icons/card.png" alt="">
-                <h2>Controle seus gastos com cartões pré-pagos VISA</h2>
-                <p>Os cartões pré-pago BPP Corp são fáceis de controlar: o colaborador só gasta o que a empresa disponibiliza para compras online ou em lojas físicas. Conte com cartões VISA aceitos em mais de 30 milhões de estabelecimentos no mundo todo, incluindo sites e aplicativos!</p>
+        </div>
+        <div class="row compare__container">
+            <div class="col-lg-12 compare__heading">
+                <h2>Compare e <strong>decida agora</strong> mesmo</h2>
+                <p>Substitua o uso do dinheiro em espécie pelo BPP Corp e ganhe segurança e praticidade, evitando roubos
+                    e<br> perdas, tendo uma plataforma única para gerir suas despesas.</p>
             </div>
-            <div class="col-lg-4 tips__block">
-                <img src="assets/img/icons/id.png" alt="">
-                <h2>Conta digital gratuita e exclusiva para sua empresa</h2>
-                <p>Oferecemos uma conta digital gratuita especialmente para gerenciar despesas. Transfira o saldo de sua conta para os cartões de maneira instantânea, sempre livre de taxas de manutenção ou de juros.</p>
+            <div class="col-lg-6 compare__money">
+                <img src="assets/img/icons/money.png" alt="">
+                <h2>Dinheiro em papel</h2>
+                <ul>
+                    <li>Saque de dinheiro em papel para as despesas</li>
+                    <li>Várias notas fiscais em papel</li>
+                    <li>Diversas planilhas de gestão</li>
+                    <li>Problemas de contabilização dos gastos</li>
+                    <li>Não tem segurança</li>
+                </ul>
             </div>
+            <div class="col-lg-6 compare__bpp">
+                <img src="assets/img/icons/bpp.png" alt="">
+                <h2>BPP Corp</h2>
+                <ul>
+                    <li>Faça pagamentos com o BPP Corp sem precisar tirar dinheiro</li>
+                    <li>Prestação de contas sem papel, via app</li>
+                    <li>Relatórios completos em um único lugar</li>
+                    <li>Controle dos gastos com cartões pré-pago</li>
+                    <li>Segurança total do seu dinheiro</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row banner-cta__container">
+            <div class="col-lg-12 banner__cta-text">
+                <h2>A sua gestão financeira nunca foi tão fácil.</h2>
+                <p>Conte com as soluções do BPP Corp e facilite o controle dos gastos da sua empresa agora mesmo.</p>
+                <a href="#">PEÇA JÁ SUA PROPOSTA</a>
+                <p>Proposta <strong>grátis</strong> e sem compromisso.</p>
+            </div>
+        </div>
+        <div class="row posts__container">
+            <div class="col-lg-12">
+                <h2>Últimos posts</h2>
+            </div>
+            <!--            
+            $args = array( 'numberposts' => '3' );
+            $recent_posts = wp_get_recent_posts( $args );
+            foreach( $recent_posts as $recent ){
+                echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .$recent["post_title"].'</a> </li> ';
+            }
+            -->
+        </div>
+        <div class="row feedback__container">
+            <div class="col-lg-12 feedback__block">
+                <h2>Esta página foi útil?</h2>
+                <div class="feddback__icon-block">
+                    <a href="#"><img src="assets/img/icons/sad.png" alt=""></a>
+                    <a href="#"><img src="assets/img/icons/indifferent.png" alt=""></a>
+                    <a href="#"><img src="assets/img/icons/happiness1.png" alt=""></a>
+                    <a href="#"><img src="assets/img/icons/happiness2.png" alt=""></a>
+                </div>
             </div>
         </div>
     </div>
